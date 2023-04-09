4c1f703051e69432b9ee9419d949bd7feff930a9
Nitesh Kumar <21bcs1673@cuchd.in>
2023-04-09 11:26:46 +0530

    Nitesh

--- app/src/main/java/com/example/file_manager/MainActivity.java
+++ app/src/main/java/com/example/file_manager/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.file_manager;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -11,19 +12,45 @@ import android.Manifest;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private Button uploadButton;
+    private Uri fileUri;
+
+//    ActivityMainBinding binding;
+    Uri uri;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseStorage firebaseStorage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
 
         MaterialButton storageBtn = findViewById(R.id.storage_btn);
 
@@ -59,6 +86,15 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},111);
         } else {
             Toast.makeText(MainActivity.this,"Storage permission is requires,please allow from settings",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 101 && resultCode == RESULT_OK) {
+            uri  = data.getData();
         }
     }
 }
--- app/src/main/java/com/example/file_manager/Myadapter.java
+++ app/src/main/java/com/example/file_manager/Myadapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.storage.StorageManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
@@ -16,10 +19,23 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
+
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private Button uploadButton;
+    private Uri fileUri;
+    private TextView textView;
 
     Context context;
     File[] filesAndFolders;
@@ -27,25 +43,34 @@ public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
     public Myadapter(Context context, File[] filesAndFolders) {
         this.context = context;
         this.filesAndFolders = filesAndFolders;
+
     }
+
+    String sourceFilePath = "/sdcard/Documents/myfile.txt";
+    String destFilePath = "/sdcard/Downloads/myfile.txt";
+//    copyFile(sourceFilePath, destFilePath);
+
 
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view  = LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
         return new ViewHolder(view);
 
+
     }
+
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
+
         File selectedFile = filesAndFolders[position];
         holder.textView.setText(selectedFile.getName());
 
-        if(selectedFile.isDirectory()){
+        if (selectedFile.isDirectory()) {
             holder.imageView.setImageResource(R.drawable.foldericon);
-        }else{
+        } else {
             holder.imageView.setImageResource(R.drawable.docicon);
         }
 
@@ -68,8 +93,8 @@ public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
                         intent.setDataAndType(Uri.parse(selectedFile.getAbsolutePath()), type);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         context.startActivity(intent);
-                    } catch (Exception e){
-                        Toast.makeText(context.getApplicationContext(),"Cannot open the file",Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(context.getApplicationContext(), "Cannot open the file", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -80,29 +105,41 @@ public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
             public boolean onLongClick(View v) {
 
 
-                PopupMenu popupMenu = new PopupMenu(context,v);
-                popupMenu.getMenu().add("DELETE");
-                popupMenu.getMenu().add("MOVE");
-                popupMenu.getMenu().add("RENAME");
+                PopupMenu popupMenu = new PopupMenu(context, v);
+                popupMenu.getMenu().add("Delete");
+                popupMenu.getMenu().add("Move");
+                popupMenu.getMenu().add("Rename");
+                popupMenu.getMenu().add("Save Locally");
+                popupMenu.getMenu().add("Save as Confidential");
 
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getTitle().equals("DELETE")){
-                           boolean deleted =  selectedFile.delete();
-                           if (deleted){
-                               Toast.makeText(context.getApplicationContext(),"DELETED",Toast.LENGTH_SHORT).show();
-                               v.setVisibility(View.GONE);
-                           }
+                        if (item.getTitle().equals("DELETE")) {
+                            boolean deleted = selectedFile.delete();
+                            if (deleted) {
+                                Toast.makeText(context.getApplicationContext(), "DELETED", Toast.LENGTH_SHORT).show();
+                                v.setVisibility(View.GONE);
+                            }
 
                         }
-                        if (item.getTitle().equals("MOVE")){
-                            Toast.makeText(context.getApplicationContext(),"MOVED",Toast.LENGTH_SHORT).show();
+                        if (item.getTitle().equals("MOVE")) {
+                            Toast.makeText(context.getApplicationContext(), "MOVED", Toast.LENGTH_SHORT).show();
 
                         }
-                        if (item.getTitle().equals("RENAME")){
-                            Toast.makeText(context.getApplicationContext(),"RENAME",Toast.LENGTH_SHORT).show();
+                        if (item.getTitle().equals("RENAME")) {
+                            Toast.makeText(context.getApplicationContext(), "RENAME", Toast.LENGTH_SHORT).show();
 
+                        }
+
+                        if (item.getTitle().equals("Save Locally")) {
+                            Toast.makeText(context.getApplicationContext(), "Saved Locally", Toast.LENGTH_SHORT).show();
+
+                        }
+                        if (item.getTitle().equals("Save as Confidential")) {
+                            Toast.makeText(context.getApplicationContext(), "Saved To Confidential", Toast.LENGTH_SHORT).show();
+
+//                            cpy("test001");
                         }
                         return true;
                     }
@@ -130,8 +167,34 @@ public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
             textView = itemView.findViewById(R.id.file_name_text_view);
             imageView = itemView.findViewById(R.id.icon_view);
         }
+
+    }
+
+    public static void copyFile(String sourceFilePath, String destFilePath) throws IOException {
+        File sourceFile = new File(sourceFilePath);
+        File destFile = new File(destFilePath);
+
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        try {
+            FileInputStream inputStream = new FileInputStream(sourceFile);
+            FileOutputStream outputStream = new FileOutputStream(destFile);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
-
-
 
